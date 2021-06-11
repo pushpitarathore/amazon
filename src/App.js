8b4cf803cff16a412d6e87e88dc7e9c034733423
unknown <pushpitarathore2106@gmail.com>
2021-06-11 15:08:32 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div className="p_header_top"></div>
+        <div className="p_header_bottom"></div>
       </header>
+      <div className="p_main position-relative">
+        <div className="p_main1">
+          <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src="./images/slider/1.jpg" className="d-block " alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/slider/2.jpg" className="d-block " alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/slider/3.jpg" className="d-block " alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/slider/4.jpg" className="d-block " alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/slider/5.jpg" className="d-block" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/slider/6.jpg" className="d-block" alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/slider/7.jpg" className="d-block " alt="..." />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/slider/8.jpg" className="d-block" alt="..." />
+              </div>
+            </div>
+            <button className="carousel-control-prev h-50" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next h-50" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+
+        </div>
+        <div className="p_main2 w-100 position-absolute"></div>
+      </div>
+      <footer> </footer>
+
     </div>
   );
 }
